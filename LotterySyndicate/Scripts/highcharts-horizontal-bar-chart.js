c3d6408a39a6afdf4452cd6384e4d83c1250eb68
Tom Andrews <tom.andrews@c5alliance.com>
2017-10-13 11:02:49 +0100

    Added highcharts - not working

---
+++ LotterySyndicate/Scripts/highcharts-horizontal-bar-chart.js
@@ -0,0 +1,22 @@
+﻿
+
+
+$(document).ready(function () {
+
+    var url = "https://localhost:44327/api/webapi";
+
+
+    var options = {
+        chart: {
+            renderTo: 'container',
+            type: 'spline'
+        },
+        series: [{}]
+    };
+
+    $.getJSON(url, function(data) {
+        options.series[0].data = data;
+        var chart = new Highcharts.Chart(options);
+    });
+
+});
